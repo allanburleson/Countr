84a8a908fd69d273416624e855820001e4599295
Lukas Kollmer <lukas.kollmer@me.com>
2014-11-30 10:25:03 +0100

    Initial Commit

---
+++ CountrTests/CountrTests.swift
@@ -0,0 +1,36 @@
+//
+//  CountrTests.swift
+//  CountrTests
+//
+//  Created by Lukas Kollmer on 30/11/14.
+//  Copyright (c) 2014 Lukas Kollmer. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class CountrTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
