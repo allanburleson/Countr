d0f73bd7c24bd2d8d28956731d33bf28e525a66a
Lukas Kollmer <lukas@kollmer.me>
2015-05-08 19:47:38 +0200

    refresh the app badge on app exit/termination

--- Countr/AppDelegate.swift
+++ Countr/AppDelegate.swift
@@ -223,6 +223,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+        setAppBadge()
     }
 
     func applicationDidEnterBackground(application: UIApplication) {
@@ -232,6 +233,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        setAppBadge()
     }
 
     func applicationDidBecomeActive(application: UIApplication) {
@@ -242,6 +244,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         // Saves changes in the application's managed object context before the application terminates.
         self.model.saveContext()
+        
+        setAppBadge()
     }
     
 
